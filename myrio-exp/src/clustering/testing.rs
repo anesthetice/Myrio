6ad7adba581090570ffc728143a0123f2da437d4
anesthetice <118751106+anesthetice@users.noreply.github.com>
2025-07-23 17:34:26 +0200

    optimization to find somewhat ideal T1, T2 values seems promising, basic example seems to work well

--- myrio-exp/src/clustering/testing.rs
+++ myrio-exp/src/clustering/testing.rs
@@ -1,22 +1,130 @@
-use argmin::core::CostFunction;
+use std::collections::HashMap;
 
-enum SimilarityFunction {
-    Cosine,
-    Overlap,
+use argmin::{
+    core::{CostFunction, Executor, Gradient},
+    solver::{linesearch::MoreThuenteLineSearch, quasinewton::LBFGS},
+};
+use itertools::Itertools;
+use myrio_core::MyrSeq;
+use ndarray::{Array1, array};
+use rand::SeedableRng;
+
+use crate::simseq::generate_pseudo_amplicon;
+
+const EPS_GRAD: f64 = 1e-2;
+
+struct ClusterAlgorithm {
+    k: usize,
+    myrseqs: Vec<MyrSeq>,
 }
 
-struct ClusterAlgorithm {}
+impl ClusterAlgorithm {
+    fn cluster(
+        &self,
+        t1_cutoff: f64,
+        t2_cutoff: f64,
+    ) -> Vec<Vec<MyrSeq>> {
+        super::method_one(self.myrseqs.clone(), self.k, t1_cutoff, t2_cutoff, super::cosine_similarity)
+            .unwrap()
+    }
+}
+
+impl ClusterAlgorithm {
+    fn cost_function(
+        &self,
+        t1_cutoff: f64,
+        t2_cutoff: f64,
+    ) -> f64 {
+        let mut cost: f64 = 0.0;
+        let clusters = self.cluster(t1_cutoff, t2_cutoff);
+
+        for (idx, cluster) in clusters.into_iter().enumerate() {
+            cost += idx as f64; // so that having many clusters get exceedingly worst
+            let mut id_count_map: HashMap<&str, usize> = HashMap::new();
+            for myrseq in cluster.iter() {
+                let count_ref = id_count_map.entry(myrseq.id.as_str()).or_default();
+                *count_ref += 1;
+            }
+            let counts = id_count_map.values().copied().collect_vec();
+            let max_count = *counts.iter().max().unwrap();
+            cost += ((counts.iter().sum::<usize>() - max_count) as f64) * 3.0;
+        }
+
+        cost
+    }
+}
 
 impl CostFunction for ClusterAlgorithm {
-    type Param = (f64, f64, SimilarityFunction);
     type Output = f64;
+    type Param = Array1<f64>;
 
     fn cost(
         &self,
         param: &Self::Param,
     ) -> Result<Self::Output, anyhow::Error> {
-        unimplemented!()
+        let t1_cutoff = param[0];
+        let t2_cutoff = param[1];
+        Ok(self.cost_function(t1_cutoff, t2_cutoff))
     }
 }
 
-fn generate_random_seqs() {}
+impl Gradient for ClusterAlgorithm {
+    type Gradient = Array1<f64>;
+    type Param = Array1<f64>;
+
+    fn gradient(
+        &self,
+        param: &Self::Param,
+    ) -> Result<Self::Gradient, anyhow::Error> {
+        let t1 = param[0];
+        let t2 = param[1];
+        let base = self.cost_function(t1, t2);
+        Ok(array![
+            (self.cost_function(t1 + EPS_GRAD, t2) - base) / EPS_GRAD,
+            (self.cost_function(t1, t2 + EPS_GRAD) - base) / EPS_GRAD,
+        ])
+    }
+}
+
+pub fn run() -> anyhow::Result<()> {
+    // Define cost function
+    let cost = ClusterAlgorithm {
+        myrseqs: MyrSeq::decode_vec_from_file("./ignore/argmin_myrseqs.bin").unwrap(),
+        k: 5,
+    };
+
+    let init_param: Array1<f64> = array![0.66, 0.66];
+
+    let init_grad = cost.gradient(&init_param)?;
+    println!("Initial gradient: {init_grad:?}");
+
+    let linesearch = MoreThuenteLineSearch::new().with_c(1e-4, 0.9)?;
+
+    let solver = LBFGS::new(linesearch, 10); // 10 is memory size
+
+    let res = Executor::new(cost, solver)
+        .configure(|state| state.param(init_param.clone()).max_iters(100))
+        .run()?;
+
+    // Print result
+    println!("{res}");
+    Ok(())
+}
+
+pub fn _generate_and_save_random_sequences_for_cluster_testing() {
+    let mut rng = rand::rngs::StdRng::from_os_rng();
+
+    let myrseqs: Vec<MyrSeq> = [
+        // ITS
+        generate_pseudo_amplicon(&mut rng, 588, 150, "1"),
+        // matK
+        generate_pseudo_amplicon(&mut rng, 1500, 150, "2"),
+        // rbcL
+        generate_pseudo_amplicon(&mut rng, 1431, 150, "3"),
+        // trnH-psbA
+        generate_pseudo_amplicon(&mut rng, 1058, 150, "4"),
+    ]
+    .concat();
+
+    MyrSeq::encode_vec_to_file("./ignore/argmin_myrseqs.bin", &myrseqs, 17).unwrap();
+}
--- myrio-exp/src/main.rs
+++ myrio-exp/src/main.rs
@@ -13,7 +13,34 @@ use myrio_core::MyrSeq;
 use rand::{RngCore, SeedableRng, rngs::SmallRng};
 use rand_distr::{Distribution, Poisson};
 
+fn main3() {
+    clustering::testing::run().unwrap();
+}
+
 fn main() -> anyhow::Result<()> {
+    let myrseqs = MyrSeq::decode_vec_from_file("./ignore/argmin_myrseqs.bin")?;
+    let t1_cutoff = 0.6412506471605364;
+    let t2_cutoff = 0.6449838846650066;
+
+    let clusters =
+        clustering::method_one(myrseqs.clone(), 5, t1_cutoff, t2_cutoff, clustering::cosine_similarity)?;
+
+    for (idx, cluster) in clusters.iter().enumerate() {
+        ///*
+        let mut id_count_map: HashMap<&str, usize> = HashMap::new();
+        for myrseq in cluster.iter() {
+            let count_ref = id_count_map.entry(myrseq.id.as_str()).or_default();
+            *count_ref += 1;
+        }
+        println!("cluster {idx}: {id_count_map:?}")
+        //*/
+        //println!("cluster {idx}: {}", cluster.len());
+    }
+
+    Ok(())
+}
+
+fn main2() -> anyhow::Result<()> {
     /*
     let myrseqs: Vec<MyrSeq> = myrio_core::io::read_fastq(
         //"./ignore/Cymbopogon_Citrus_Qiagen_matk_rbcL_psbA-trnH_ITS_barcode3.fastq",
